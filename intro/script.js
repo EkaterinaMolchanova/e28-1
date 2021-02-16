c8bfcc2aff6f998273d3e44ddb074d3e58a33994
Susan Buck <susan@susanbuck.net>
2021-02-16 15:38:12 -0500

    Week 4 Intro example continued...

--- intro/script.js
+++ intro/script.js
@@ -7,10 +7,23 @@ const Game = {
             guess: null,
             guesses: [],
             correct: false,
+            // The following two properties were added just to demo
+            // some array/object rendering. They are not actually part of the game/app.
+            settings: {
+                max: 200,
+                type: 'numeric',
+                guessLimit: 15,
+            },
+            guessesDetailed: [
+                { guess: 15, result: 'low' },
+                { guess: 60, result: 'high' },
+                { guess: 50, result: 'low' },
+            ]
         }
     },
     methods: {
-        addGuess() {
+        addGuess(event) {
+            console.log(event);
             this.guesses.push(this.guess);
             this.correct = this.guess == this.mysteryNumber;
         },
