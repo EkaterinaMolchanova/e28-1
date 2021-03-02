4f03ec5272a8b2ebabc03d46155999bbf17f3300
Susan Buck <susan@susanbuck.net>
2021-03-02 18:38:39 -0500

    Week 6 example (components)

--- week6/script.js
+++ week6/script.js
@@ -1,9 +1,43 @@
-const Example = {
+const App = {
+    data() {
+        return {
+            rounds: [
+                { number: 1, winner: 'Player', coin: 'heads', choice: 'heads' },
+                { number: 2, winner: 'Computer', coin: 'tails', choice: 'heads' },
+                { number: 3, winner: 'Computer', coin: 'heads', choice: 'tails' },
+                { number: 4, winner: 'Player', coin: 'tails', choice: 'tails' },
+            ]
+        }
+    },
+    methods: {
+        deleteRound(number) {
+            this.rounds = this.rounds.filter(round => round.number != number);
+        }
+    }
+}
+
+const RoundDetail = {
+    name: 'RoundDetail',
+    props: {
+        number: {
+            type: Number,
+            default: 0
+        },
+        winner: {
+            type: String
+        },
+        choice: {
+            type: String
+        }
+    },
     data() {
         return {
         }
     },
+    template: '#round-detail'
 }
 
 // Root Vue instance
-const app = Vue.createApp(Example).mount('#app');
+const app = Vue.createApp(App)
+app.component('round-detail', RoundDetail);
+app.mount('#app');
